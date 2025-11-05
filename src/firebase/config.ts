7cdc6c758072e2c9e5a19aed6575c7b7cd93e6e5
ahmed joudi <ahmed.joudi9b7@gmail.com>
2025-11-05 09:42:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3589955672-bcd9f",
+  "appId": "1:561075602288:web:511481c38f1702fc9a85b5",
+  "apiKey": "AIzaSyD-d2XX8g_GS8UIpf90tiQeTTWS5nxVzlU",
+  "authDomain": "studio-3589955672-bcd9f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "561075602288"
+};
